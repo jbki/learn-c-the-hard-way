1c971306344064cdbeedc0a3fdf489843ad05380
crandonriordan <crandon.riordan@gmail.com>
2020-06-24 07:21:28 -0500

    add in ex3

---
+++ ex3/ex3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) 
+{
+	int age = 10;
+	double height = 72.3733;
+	char *name = "Crandon";
+
+	printf("I am %d years old.\n", age);
+	printf("I am %6.1f inches tall.\n", height);
+	printf("My name is %s.\n", name);
+
+	return 0;
+}
